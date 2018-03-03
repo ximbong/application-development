911626ee1374351a438d226d6d7418a9401aca65
Hai Truong <ximbong91023@users.noreply.github.com>
2018-03-03 10:53:44 +0200

    Change index css file.
    Link fontawesome v4.7, fix input background default color, enable pressing enter to submit form.

--- WebApplication1/web/index.js
+++ WebApplication1/web/index.js
@@ -1,24 +1,35 @@
- document.addEventListener("DOMContentLoaded", function(event) {
-0
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  var data = {};
+
+  document.getElementById('form').addEventListener('input',function(){
+    data.username=document.getElementById('username').value;
+    data.password=document.getElementById('password').value;
+  })
 
 
+  var submit = function(){
+    let  url = "http://localhost:8080/WebApplication1/ws/users?username="+data.username+"&password=" +data.password;
+    fetch(url, {
+      method: "POST",
+      cache: 'no-cache'
+    }).then(response => response.json())
+    .then(result => (result.length===0)? alert('Wrong username or password.'):(
+      window.location.replace("http://localhost:8080/WebApplication1/MainPage.html"),
+      localStorage.setItem("user", JSON.stringify(result[0]))
+    ))
+  }
 
-var data = {};
-
-document.getElementById('form').addEventListener('input',function(){
-  data.username=document.getElementById('username').value;
-  data.password=document.getElementById('password').value;
-})
-
-document.getElementById('submit').addEventListener('click',function(){
-    console.log(data);
-  var url = "http://localhost:8080/WebApplication1/ws/users?username="+data.username+"&password=" +data.password;
-  fetch(url, {
-    method: "POST"
-  }).then(response => response.json())
-     .then(result => (result.length===0)?alert('Wrong username or password.'): window.location.replace("http://localhost:8080/WebApplication1/MainPage.html"))
-      .then(result => localStorage.setItem("user", result[0]))
-})
-
-
+  document.getElementById('submit').addEventListener('click',function(){
+    submit();
   });
+
+
+  document.addEventListener("keypress", function(e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) {
+      submit();
+    }
+  });
+
+})
