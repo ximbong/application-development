64c1dbfc90326b346cb801ac2f14d9f1f898310b
Hai Truong <ximbong91023@users.noreply.github.com>
2018-03-07 02:46:13 +0200

    Fix small bugs that project cached the old EJB.
    By clearing the cache folder and run the app again.

--- WebApplication1/build/generated-sources/ap-source-output/Database/Announcement_.java
+++ WebApplication1/build/generated-sources/ap-source-output/Database/Announcement_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-07T02:17:29")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-07T02:44:42")
 @StaticMetamodel(Announcement.class)
 public class Announcement_ { 
 
--- WebApplication1/build/generated-sources/ap-source-output/Database/Department_.java
+++ WebApplication1/build/generated-sources/ap-source-output/Database/Department_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-07T02:17:29")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-07T02:44:42")
 @StaticMetamodel(Department.class)
 public class Department_ { 
 
