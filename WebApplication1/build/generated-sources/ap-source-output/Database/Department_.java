736b98bde35379dabc33a97377e1c7c6f93ec24a
Hai Truong <ximbong91023@users.noreply.github.com>
2018-03-07 11:44:41 +0200

    Change localhost to remote host
    By changing the link path.

--- WebApplication1/build/generated-sources/ap-source-output/Database/Department_.java
+++ WebApplication1/build/generated-sources/ap-source-output/Database/Department_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-07T11:05:38")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-07T11:32:43")
 @StaticMetamodel(Department.class)
 public class Department_ { 
 
